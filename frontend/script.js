15f9ed7e25cf2c7ebc293e49f8b85c6a781faebb
Juyeong Park <juyeongpark@ju0s-MacBook-Air.local>
2023-01-13 18:20:11 -0800

    changed the url

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         const message = $('#chatbot-input').val();
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:3000/chatbot',
+            url: 'https://comp4537-lab2-qtu6.onrender.com/chatboat',
             data: { message: message },
             success: function (response) {
                 let newMessage = $('<div>', { class: 'message' }).text(response.text);
